c6f51ddd87159be51211ecdd6065b4498e0b3f94
Sebastian_rendon <srendon521@gmail.com>
2026-02-13 09:28:20 -0500

    Se agregaron los archivos para las pruebas

---
+++ Pruebas_unitarias_Proyecto/logica_liquidador.py
@@ -0,0 +1,12 @@
+
+def calcular_salario(salario, horas_extra, bonificaciones, auxilios, salud, pension, impuesto_dinero):
+    """
+    salario: El salario base del trabajador
+    horas_extra: En caso de que el trabajador haya trabajado realizado horas extra, caso contrario ingresa 0
+    bonificaciones: En caso de que el trabajador obtuviera bonificaciones, caso contrario ingresa 0
+    auxilios: En caso de que el trabajador obtuviera auxilios, caso contrario ingresa 0
+    salud: Porcentaje de salud del trabajador, en Colombia permanece generalmente sin cambios expresado en decimal (ejemplo: 4% ingresar 0.04)
+    pension: Porcentaje de pension del trabajador, en Colombia permanece generalmente sin cambios expresado en decimal (ejemplo: 4% ingresar 0.04)
+    impuesto_dinero: Cualquier tipo de impuesto aplicado, expresado en dinero (un entero)
+    """
+    return ()
