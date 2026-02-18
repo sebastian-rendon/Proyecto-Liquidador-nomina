1acfd91da1d8d029eecfc4065150e06283946ef6
Juan Gomez <juanc1306gg@hotmail.com>
2026-02-18 09:09:18 -0500

    Se movieron las carpetas de prueba a la raiz

--- test_liquidador.py
+++ test_liquidador.py
@@ -24,6 +24,26 @@ class TestCalculoSalario(unittest.TestCase):
         #verificar la salida
         self.assertEqual(salario_calculado, salario_esperado)
 
+    def test_normal2_salario(self):
+
+        #entradas
+        salario = 6200000
+        horas_extra = 100000
+        bonificaciones = 100000
+        comisiones = 20000
+        auxilios = 0
+        salud = 0.04
+        pension = 0.04
+        impuesto_dinero = 62000
+
+        #proceso
+        salario_calculado = logica_liquidador.calcular_salario(salario, horas_extra, bonificaciones, comisiones, auxilios, salud, pension, impuesto_dinero)
+
+        #Datos de salida esperados
+        salario_esperado = 5862000
+
+        #verificar la salida
+        self.assertEqual(salario_calculado, salario_esperado)
 
 if __name__ == '__main__':
     unittest.main()
