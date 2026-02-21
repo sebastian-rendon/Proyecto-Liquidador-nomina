97bac99577cb967c79119594e4fb320f02c7bdbe
Sebastian Rendon <srendon521@gmail.com>
2026-02-21 00:24:45 -0500

    Se verificó el correcto funcionamiento

--- consola_liquidador.py
+++ consola_liquidador.py
@@ -1,24 +1,28 @@
 import logica_liquidador
 
-salario = float(input("ingrese el valor del salario mensual: "))
-horas_extra = input("ingrese el valor de las horas extra: ")
-bonificaciones = input("ingrese el valor de las bonificaciones: ")
-comisiones = input("ingrese el valor de las comisiones: ")
-auxilios = input("ingrese el valor de los auxilios: ")
+try:
+    salario = float(input("Ingrese el valor del salario mensual: "))
+    horas_extra = float(input("Ingrese el valor de las horas extra: "))
+    bonificaciones = float(input("Ingrese el valor de las bonificaciones: "))
+    comisiones = float(input("Ingrese el valor de las comisiones: "))
+    auxilios = float(input("Ingrese el valor de los auxilios: "))
 
-salud = float(input("ingrese el valor de la salud que usted paga (en decimal): "))
-pension = float(input("ingrese el valor de la pension que usted paga (en decimal):"))
-impuesto_dinero = input("ingrese el valor de los impuestos que usted paga: ")
+    salud = float(input("Ingrese el valor de la salud que usted paga (en decimal): "))
+    pension = float(input("Ingrese el valor de la pension que usted paga (en decimal): "))
+    impuesto_dinero = float(input("Ingrese el valor de los impuestos que usted paga: "))
 
-salario_neto = logica_liquidador.calcular_salario(
-    salario,
-    horas_extra,
-    bonificaciones,
-    comisiones,
-    auxilios,
-    salud,
-    pension,
-    impuesto_dinero
-)
+    salario_neto = logica_liquidador.calcular_salario(
+        salario,
+        horas_extra,
+        bonificaciones,
+        comisiones,
+        auxilios,
+        salud,
+        pension,
+        impuesto_dinero
+    )
 
-print("El salario neto es: ", salario_neto)
+    print("El salario neto es:", salario_neto)
+
+except Exception as e:
+    print("Ocurrió un error:", e)
